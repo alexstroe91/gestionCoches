81b33a59bbdfa473b20d34b40bf7ab1aaf4131e1
alexstroe91 <alexstroe91@gmail.com>
2021-11-19 19:32:52 +0100

    Llamar al metodo de leer fichero en el load del formulario

--- gestionCoches/gestionCoches/Formularios/frmListado.vb
+++ gestionCoches/gestionCoches/Formularios/frmListado.vb
@@ -1,3 +1,8 @@
 ﻿Public Class frmListado
+    Dim metodos As New metodosListado
+    Private Sub frmListado_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        metodos.leerFichero(Me)
+
+    End Sub
 End Class
