c449565f37833ce67d95da8c64f84127cec87b65
alexstroe91 <alexstroe91@gmail.com>
2021-11-19 19:31:43 +0100

    Abrir el fromulario de listado al pulsar sobre el boton

--- gestionCoches/gestionCoches/Formularios/frmPrincipal.vb
+++ gestionCoches/gestionCoches/Formularios/frmPrincipal.vb
@@ -1,12 +1,13 @@
 ﻿Public Class frmPrincipal
     Dim metodos As New metodosAltas
 
-    Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
     Private Sub btnAltas_Click(sender As Object, e As EventArgs) Handles btnAltas.Click
         Dim frm As New frmAltas
+        frm.Show()
+    End Sub
+
+    Private Sub btnListado_Click(sender As Object, e As EventArgs) Handles btnListado.Click
+        Dim frm As New frmListado
         frm.Show()
     End Sub
 End Class
